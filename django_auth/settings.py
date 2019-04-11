67a9e51aea148b1a59c7e16f5a69d0ada8498f8f
Joke Heyndels <jo_hannah@outlook.com>
2019-04-11 16:15:39 +0200

    Undo last change, add static_root = "../static"

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '../static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
